eb79006895fd91c5c55325aa0307d299bd2be003
zoltan.orosz <zoltan.orosz@leopoly.com>
2023-04-01 13:35:13 +0200

    Create ASP.NET Core MVC project

---
+++ CarLinkServices.Web/CarLinkServices.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarLinkServices.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
